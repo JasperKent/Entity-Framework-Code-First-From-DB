8b30015d26ef2d2929b460577a3b198a971481ef
Jasper Kent <jasper_kent@hotmail.com>
2020-09-25 08:49:52 +0100

    Add project files.

---
+++ DBFirstLibrary/Books.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DBFirstLibrary
+{
+    public partial class Books
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public long YearOfPublication { get; set; }
+        public long? AuthorId { get; set; }
+
+        public virtual Authors Author { get; set; }
+    }
+}
